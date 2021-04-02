05bd41265cd9cf2a5c782f9c177554fae80c798b
Aniket-star2 <81812037+Aniket-star2@users.noreply.github.com>
2021-04-02 17:49:44 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,39 @@
+class paper {
+    constructor(x, y,r) {
+      var options = {
+        isStatic:false,
+        'density':0.5,
+        'friction': 0,
+        'restitution':0.3
+      };
+      this.x =  x;
+      this.y = y;
+      this.r = r;
+      this.body = Bodies.circle(this.x,this.y,this.r,options)
+      this.image = loadImage("paper.png")
+      World.add(world,this.body)
+    };
+    display(){
+      var pos = this.body.position;
+  
+      push();
+      translate(pos.x, pos.y);
+      strokeWeight(3);
+      imageMode(CENTER)
+      image(this.image,0, 0, this.r);
+      pop();
+    };
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
